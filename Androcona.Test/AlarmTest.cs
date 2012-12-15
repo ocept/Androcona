5c9b89c0f0d836cdd9ec14489bded49c75fcb479
Oliver Warden <oliverwarden@gmail.com>
2012-12-15 12:30:26 +0000

    Create test project

---
+++ Androcona.Test/AlarmTest.cs
@@ -0,0 +1,83 @@
+﻿using Androcona;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Androcona.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for AlarmTest and is intended
+    ///to contain all AlarmTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class AlarmTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Alarm Constructor
+        ///</summary>
+        [TestMethod()]
+        public void AlarmConstructorTest()
+        {
+            AlarmSettings settings = new AlarmSettings();
+            System.DateTime testDate = System.DateTime.Now.AddHours(1);
+            settings.time = testDate;
+            settings.type = AlarmSettings.aType.Alarm;
+            Alarm target = new Alarm(settings);
+            Assert.IsNotNull(target);
+            Assert.AreEqual(AlarmSettings.aType.Alarm, target.AlarmType);
+            Assert.AreEqual(testDate, target.AlarmTime);
+
+
+        }
+    }
+}
--- Androcona.Test/Properties/AssemblyInfo.cs
+++ Androcona.Test/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Androcona")]
+[assembly: AssemblyTitle("Androcona.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Androcona")]
+[assembly: AssemblyProduct("Androcona.Test")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1431502-87cc-4a30-ba3e-b3bd8acb8906")]
+[assembly: Guid("4c72b5c3-03f3-4e64-bdb3-2e7cd81b4dcf")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,10 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Androcona.Test")]
